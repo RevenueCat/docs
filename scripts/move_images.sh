fb157f61fa454b99e7be1aafc566bdb2a938e3a8
Cesar de la Vega <cesarvegaro@gmail.com>
2024-02-06 10:57:27 +0100

    skip badge urls and add md5

--- scripts/move_images.sh
+++ scripts/move_images.sh
@@ -14,16 +14,29 @@ process_files() {
     while IFS= read -r line; do
         # Extract URL, ignoring alt text if present
         url=$(echo "$line" | sed -E 's/.*\((https?:\/\/[^ ]+).*\)/\1/')
-        filename=$(basename "$url")
-        # Download the image if it does not exist
-        if [ ! -f "$static_dir/$filename" ]; then
-            echo "📥 Downloading $url to $static_dir/$filename"
-            wget -q "$url" -P "$static_dir"
+
+        # Skip images from https://img.shields.io/
+        if echo "$url" | grep -q "https://img.shields.io/"; then
+            echo "⏭ Skipping badge $url"
+            continue
         fi
+
+        filename=$(basename "$url")
+        # Create a unique filename by appending a hash of the URL to the original filename
+        # This prevents different images with the same name (like giphy.gif images) from being overwritten
+        unique_suffix=$(echo "$url" | md5sum | cut -d' ' -f1)
+        unique_filename="${filename%.*}_$unique_suffix.${filename##*.}"
+
+        # Download the image if it does not exist
+        if [ ! -f "$static_dir/$unique_filename" ]; then # Fixed typo in variable name here
+            echo "📥 Downloading $url to $static_dir/$unique_filename"
+            wget -q "$url" -O "$static_dir/$unique_filename"
+        fi
+
         # Update the file to reference the local copy of the image
-        replacement="$images_dir/$filename"
+        replacement="/$images_dir/$unique_filename"
         echo "🔄 Replacing $url with $replacement in $file"
-        sed -i.bak "s|$url|$replacement|g" "$file" && rm "${file}.bak"
+        sed -i.bak "s|$(echo "$url" | sed 's|[\&/]|\\&|g')|$replacement|g" "$file" && rm "${file}.bak"
     done < <(grep -oE '!\[.*?\]\((https?:\/\/[^ ]+).*\)' "$file")
 }
 
