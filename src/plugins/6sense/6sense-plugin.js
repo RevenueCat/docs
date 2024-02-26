e05d0efede888554e8a739cce4e69650d1eeecb2
Cody Kerns <cody@revenuecat.com>
2024-02-26 12:34:30 -0500

    add analytics

---
+++ src/plugins/6sense/6sense-plugin.js
@@ -0,0 +1,32 @@
+export default async function sixSensePlugin(context, opts) {
+  return {
+    name: "revenuecat-6sense-plugin",
+    getClientModules() {
+      const modules = ["./client-module"];
+      return modules;
+    },
+    injectHtmlTags({ content }) {
+      return {
+        headTags: [
+          {
+            tagName: "script",
+            innerHTML: `
+              window._6si = window._6si || [];
+              window._6si.push(['enableEventTracking', true]);
+              window._6si.push(['setToken', '${opts.writeKey}']);
+              window._6si.push(['setEndpoint', 'b.6sc.co']);
+              (function() {
+                var gd = document.createElement('script');
+                gd.type = 'text/javascript';
+                gd.async = true;
+                gd.src = '//j.6sc.co/6si.min.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(gd, s);
+              })();
+              `,
+          },
+        ],
+      };
+    },
+  };
+}
---
+++ src/plugins/6sense/client-module.js
@@ -0,0 +1,1 @@
+export function onRouteDidUpdate({ location, previousLocation }) {}
