1c8cab5fd3866a6f7f39d3281d04cddd10c6b333
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-02-26 09:40:55 +0100

    Make RN paywall custom offering docs clearer

--- code_blocks/tools/paywalls_rn_2.ts
+++ code_blocks/tools/paywalls_rn_2.ts
@@ -3,10 +3,18 @@ import { View } from 'react-native';
 
 import RevenueCatUI from 'react-native-purchases-ui';
 
+// Simple version
+return (
+    <View style={{ flex: 1 }}>
+        <RevenueCatUI.Paywall />
+    </View>
+);
+
+// If you need to display a specific offering:
 return (
     <View style={{ flex: 1 }}>
         <RevenueCatUI.Paywall options={{
-            offering: offering // Optional custom offering
+            offering: offering // Optional Offering object obtained through getOfferings
         }} />
     </View>
 );
--- code_blocks/tools/paywalls_rn_3.ts
+++ code_blocks/tools/paywalls_rn_3.ts
@@ -3,10 +3,20 @@ import { Text } from 'react-native';
 
 import RevenueCatUI from 'react-native-purchases-ui';
 
+// Simple version
+return (
+    <RevenueCatUI.PaywallFooterContainerView>
+        <Text>
+            Your Custom Paywall Design
+        </Text>
+    </RevenueCatUI.PaywallFooterContainerView>
+);
+
+// If you need to display a specific offering:
 return (
     <RevenueCatUI.PaywallFooterContainerView
         options={{
-            offering: offering // Optional offering parameter
+            offering: offering // Optional Offering object obtained through getOfferings
         }}
     >
         <Text>
