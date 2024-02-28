533f26dd294cc45053d7f26256c0c11af6e3578e
Ryan Glanz <ryan.glanz@revenuecat.com>
2024-02-28 09:33:26 -0600

    adds RC fonts to project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import Heading from "@theme/Heading";
-import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -19,68 +16,11 @@ function HomepageHeader() {
   const RCAsset = require("@site/static/img/landing-image.png").default;
 
   return (
-    <section className={clsx("hero hero--secondary", styles.heroSection)}>
-      <div className={clsx("container", styles.container)}>
-
-        <div className={styles.containerLeft}>
-          <Logo className={styles.logo} role="img" />
-
-          <p className={styles.smallTitle}>
-            Developer Hub
-          </p>
-          <Heading as="h1" className="text-red-700">
-            {" "}
-            Everything you need to manage in-app purchases and
-            subscriptions
-          </Heading>
-          <p className={styles.mainText}>
-            {" "}
-            Welcome to the RevenueCat Developer Hub. You'll find detailed guides
-            and documentation to help you start working with RevenueCat as quickly
-            as possible, as well as support if you get stuck. Let's jump right in!
-          </p>
-
-          <Link
-            className={clsx("button button--primary button--lg", styles.button)}
-            to="/welcome/overview/"
-          >
-            Get Started
-          </Link>
-
-          <div className={styles.containerQuickLinks}>
-            <p className={styles.quickLinksTitle}>Quick Links</p>
-            <div className={styles.quickLinks}>
-              <div className={styles.quickLink}>
-                <CodeIcon className={styles.quickLinkIcon} role="img" />
-                <a href="/docs/getting-started/quickstart" className={styles.quickLinkText}>SDK Quickstart</a>
-              </div>
-              <div className={styles.quickLink}>
-                <TruckIcon className={styles.quickLinkIcon} role="img" />
-                <a href="/docs/migrating-to-revenuecat/migrating-existing-subscriptions" className={styles.quickLinkText}>Migrating to RevenueCat</a>
-              </div>
-              <div className={styles.quickLink}>
-                <HandshakeIcon className={styles.quickLinkIcon} role="img" />
-                <a href="/docs/revenuecat-support/support-first-steps" className={styles.quickLinkText}>RevenueCat Support</a>
-              </div>
-              <div className={styles.quickLink}>
-                <ChartIcon className={styles.quickLinkIcon} role="img" />
-                <a href="/docs/dashboard-and-metrics/overview" className={styles.quickLinkText}>Dashboard & Metrics</a>
-              </div>
-              <div className={styles.quickLink}>
-                <TooolboxIcon className={styles.quickLinkIcon} role="img" />
-                <a href="https://www.revenuecat.com/docs/tools/paywalls" className={styles.quickLinkText}>Tools</a>
-              </div>
-              <div className={styles.quickLink}>
-                <CircuitIcon className={styles.quickLinkIcon} role="img" />
-                <a href="/docs/integrations/integrations" className={styles.quickLinkText}>Integrations</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.containerRight}>
-          <img src={RCAsset} />
-        </div>
+    // Hero
+    <section className="">
+      <div>
+        <Heading as="h1" className="">RevenueCat Developer Hub</Heading>
+        <p>Everything you need to implement and manage...</p>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,19 @@ module.exports = {
   darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
+    colors: {
+      primary: "#576CDB",
+      red: "#F2545B",
+      green: "#11D483",
+      yellow: "#F3E972",
+      gray: "#F6F6F6",
+    },
     extend: {
       fontFamily: {
-        sans: ["system-ui", "-apple-system", "Segoe UI", "Roboto", "Ubuntu", "Cantarell", "Noto Sans", "sans-serif", "BlinkMacSystemFont",
+        head: ["'Object Sans'", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Ubuntu", "Cantarell", "Noto Sans", "sans-serif", "BlinkMacSystemFont",
+          "'Segoe UI'", "Helvetica", "Arial", "sans-serif", "'Apple Color Emoji'",
+          "'Segoe UI Emoji'", "'Segoe UI Symbol'"],
+        body: ["'Helvetica Neue'", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Ubuntu", "Cantarell", "Noto Sans", "sans-serif", "BlinkMacSystemFont",
           "'Segoe UI'", "Helvetica", "Arial", "sans-serif", "'Apple Color Emoji'",
           "'Segoe UI Emoji'", "'Segoe UI Symbol'"]
       }
