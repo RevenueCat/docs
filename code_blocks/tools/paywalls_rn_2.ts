301a8e04f3ce4e90892be01866ba14c598948c9e
Cesar de la Vega <cesarvegaro@gmail.com>
2024-03-15 11:50:31 +0100

    add comment on when the ondismiss is called

--- code_blocks/tools/paywalls_rn_2.ts
+++ code_blocks/tools/paywalls_rn_2.ts
@@ -19,6 +19,7 @@ return (
           }}
           onDismiss={() => {
             // Dismiss the paywall, i.e. remove the view, navigate to another screen, etc.
+            // Will be called when the close button is pressed (if enabled) or when a purchase succeeds.
           }}
         />
     </View>
