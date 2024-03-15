54224248f178856c40939e5a115d9c74694fecab
Cesar de la Vega <cesarvegaro@gmail.com>
2024-03-15 18:02:05 +0100

    made more updates to match Flutter

--- code_blocks/tools/paywalls_rn_2.ts
+++ code_blocks/tools/paywalls_rn_2.ts
@@ -17,6 +17,10 @@ return (
           options={{
             offering: offering // Optional Offering object obtained through getOfferings
           }}
+          onRestoreCompleted={({customerInfo}: { customerInfo: CustomerInfo }) => {
+            // Optional listener. Called when a restore has been completed.
+            // This may be called even if no entitlements have been granted.
+          }
           onDismiss={() => {
             // Dismiss the paywall, i.e. remove the view, navigate to another screen, etc.
             // Will be called when the close button is pressed (if enabled) or when a purchase succeeds.
--- code_blocks/tools/paywalls_rn_3.ts
+++ code_blocks/tools/paywalls_rn_3.ts
@@ -18,6 +18,10 @@ return (
     options={{
       offering: offering // Optional Offering object obtained through getOfferings
     }}
+    onRestoreCompleted={({customerInfo}: { customerInfo: CustomerInfo }) => {
+      // Optional listener. Called when a restore has been completed.
+      // This may be called even if no entitlements have been granted.
+    }
     onDismiss={() => {
       // Dismiss the paywall, i.e. remove the view, navigate to another screen, etc.
     }}>
