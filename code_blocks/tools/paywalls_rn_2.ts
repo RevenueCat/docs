5efe5e7ac07d55c14f53c9cd9592f2796036e970
Cesar de la Vega <cesarvegaro@gmail.com>
2024-03-18 11:45:05 +0100

    Add listeners to the Paywall RN sample (#111)
    * add listener
    
    * add comment on when the ondismiss is called
    
    * made more updates to match Flutter

--- code_blocks/tools/paywalls_rn_2.ts
+++ code_blocks/tools/paywalls_rn_2.ts
@@ -13,8 +13,18 @@ return (
 // If you need to display a specific offering:
 return (
     <View style={{ flex: 1 }}>
-        <RevenueCatUI.Paywall options={{
+        <RevenueCatUI.Paywall
+          options={{
             offering: offering // Optional Offering object obtained through getOfferings
-        }} />
+          }}
+          onRestoreCompleted={({customerInfo}: { customerInfo: CustomerInfo }) => {
+            // Optional listener. Called when a restore has been completed.
+            // This may be called even if no entitlements have been granted.
+          }
+          onDismiss={() => {
+            // Dismiss the paywall, i.e. remove the view, navigate to another screen, etc.
+            // Will be called when the close button is pressed (if enabled) or when a purchase succeeds.
+          }}
+        />
     </View>
 );
--- code_blocks/tools/paywalls_rn_3.ts
+++ code_blocks/tools/paywalls_rn_3.ts
@@ -14,13 +14,19 @@ return (
 
 // If you need to display a specific offering:
 return (
-    <RevenueCatUI.PaywallFooterContainerView
-        options={{
-            offering: offering // Optional Offering object obtained through getOfferings
-        }}
-    >
-        <Text>
-            Your Custom Paywall Design
-        </Text>
-    </RevenueCatUI.PaywallFooterContainerView>
+  <RevenueCatUI.PaywallFooterContainerView
+    options={{
+      offering: offering // Optional Offering object obtained through getOfferings
+    }}
+    onRestoreCompleted={({customerInfo}: { customerInfo: CustomerInfo }) => {
+      // Optional listener. Called when a restore has been completed.
+      // This may be called even if no entitlements have been granted.
+    }
+    onDismiss={() => {
+      // Dismiss the paywall, i.e. remove the view, navigate to another screen, etc.
+    }}>
+      <Text>
+        Your Custom Paywall Design
+      </Text>
+  </RevenueCatUI.PaywallFooterContainerView>
 );
