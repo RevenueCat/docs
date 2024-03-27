78a4e22e03d5f684ceaa42819d67b3fdb556f540
Josh Holtz <me@joshholtz.com>
2024-03-27 16:00:46 -0500

    Update offering metadata for nested dictionary and hybrids

--- code_blocks/getting-started/displaying-products/offering-metadata_2.kt
+++
@@ -1,9 +0,0 @@
-Purchases.sharedInstance.getOfferingsWith({ error ->
-    // An error occurred
-}) { offerings ->
-    offerings.current?.let {
-        val paywallTitle = it.getMetadataString("title", default="Get Pro")
-        val paywallSubtitle = it.getMetadataString("title", default="Unlock all the features")
-        val paywallButton = it.getMetadataString("title", default="Redeem Trial")
-    }
-}
--- code_blocks/tools/offering-metadata_2.kt
+++ code_blocks/tools/offering-metadata_2.kt
@@ -2,8 +2,8 @@ Purchases.sharedInstance.getOfferingsWith({ error ->
     // An error occurred
 }) { offerings ->
     offerings.current?.let {
-        val paywallTitle = it.getMetadataString("title", default="Get Pro")
-        val paywallSubtitle = it.getMetadataString("title", default="Unlock all the features")
-        val paywallButton = it.getMetadataString("title", default="Redeem Trial")
+        val showNewBeneifts = it.metadata["show_new_beneifts"] as? Boolean
+        val title = (it.metadata["title_strings"] as? Map<*, *>)?.get("en_US")
+        val cta = (it.metadata["cta_strings"] as? Map<*, *>)?.get("en_US")
     }
 }
