5f48f2ae58b57e7e570e1e8001d658d3bd978279
Josh Holtz <me@joshholtz.com>
2024-04-04 07:48:38 -0500

    Update code_blocks/tools/offering-metadata_5.ts
    Co-authored-by: Cody Kerns <44073103+codykerns@users.noreply.github.com>

--- code_blocks/tools/offering-metadata_5.ts
+++ code_blocks/tools/offering-metadata_5.ts
@@ -2,7 +2,7 @@ const offerings = await Purchases.getOfferings();
 const current = offerings.current;
 if (current) {
   const metadata = offerings.current || {};
-  const showNewBenefits = metadata.show_new_beneifts;
+  const showNewBenefits = metadata.show_new_benefits;
   const title = metadata.title_strings["en_US"];
   const cta = metadata.cta_strings["en_US"];
 }
