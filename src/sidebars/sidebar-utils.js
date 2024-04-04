4ae119b043ea930cf7fab3b574cc1536b9488667
Ryan Glanz <ryan.glanz@revenuecat.com>
2024-04-04 15:59:40 -0500

    makes sidebar-utils more readable

--- src/sidebars/sidebar-utils.js
+++ src/sidebars/sidebar-utils.js
@@ -4,35 +4,43 @@ const checkItemType = (item) => {
   return "page";
 };
 
-const Category = ({ label, emoji, slug, items }) => ({
-  type: "category",
-  label,
-  collapsible: false,
-  customProps: { emoji },
-  items: items.map((item) => {
-    const itemType = checkItemType(item);
+const Category = ({ label, emoji, slug, items }) => {
+  return {
+    type: "category",
+    label,
+    collapsible: false,
+    customProps: { emoji },
+    items: items.map((item) => {
+      const itemType = checkItemType(item);
 
-    if (itemType === "subcategory")
-      ({
-        ...item,
-        link: {
-          type: "doc",
-          id: `${slug}/${item.link.id}`,
-        },
-        items: item.items.map((subItem) => {
-          const subItemType = checkItemType(subItem);
+      if (itemType === "subcategory") {
+        return {
+          ...item,
+          link: {
+            type: "doc",
+            id: `${slug}/${item.link.id}`,
+          },
+          items: item.items.map((subItem) => {
+            const subItemType = checkItemType(subItem);
 
-          if (subItemType === "link") return subItem;
-          if (subItemType === "page")
-            return { ...subItem, id: `${slug}/${subItem.id}` };
-        }),
-      });
+            return subItemType === "page"
+              ? { ...subItem, id: `${slug}/${subItem.id}` }
+              : subItem;
+          }),
+        };
+      }
 
-    if (itemType === "link") return item;
+      if (itemType === "page") {
+        return {
+          ...item,
+          id: `${slug}/${item.id}`,
+        };
+      }
 
-    return { ...item, id: `${slug}/${item.id}` };
-  }),
-});
+      return item;
+    }),
+  };
+};
 
 const SubCategory = ({ label, slug, items }) => ({
   type: "category",
@@ -43,10 +51,7 @@ const SubCategory = ({ label, slug, items }) => ({
   },
   items: items.map((item) => {
     const itemType = checkItemType(item);
-
-    if (itemType === "link") return item;
-
-    return { ...item, id: `${slug}/${item.id}` };
+    return itemType === "page" ? { ...item, id: `${slug}/${item.id}` } : item;
   }),
 });
 
