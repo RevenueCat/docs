bd30753e74dde2d49e2ce8f86f5dd19d882ed28b
Josh Holtz <me@joshholtz.com>
2024-04-04 07:48:19 -0500

    Update code_blocks/tools/offering-metadata_1.swift
    Co-authored-by: Cody Kerns <44073103+codykerns@users.noreply.github.com>

--- code_blocks/tools/offering-metadata_1.swift
+++ code_blocks/tools/offering-metadata_1.swift
@@ -1,6 +1,6 @@
 let offerings = try await Purchases.shared.offerings()
 if let current = offerings.current {
-    let showNewBeneifts = current.getMetadataValue(for: "show_new_beneifts", default: false)
+    let showNewBenefits = current.getMetadataValue(for: "show_new_benefits", default: false)
     let title = current.getMetadataValue(for: "title_strings", default: [:])["en_US"] as? String
     let cta = current.getMetadataValue(for: "cta_strings", default: [:])["en_US"] as? String
 }
