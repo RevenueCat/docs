ee436d90a00d6f853fcddf6a6368c5ba09f80e1c
Josh Holtz <me@joshholtz.com>
2024-04-04 07:48:23 -0500

    Update code_blocks/tools/offering-metadata_2.kt
    Co-authored-by: Cody Kerns <44073103+codykerns@users.noreply.github.com>

--- code_blocks/tools/offering-metadata_2.kt
+++ code_blocks/tools/offering-metadata_2.kt
@@ -2,7 +2,7 @@ Purchases.sharedInstance.getOfferingsWith({ error ->
     // An error occurred
 }) { offerings ->
     offerings.current?.let {
-        val showNewBeneifts = it.metadata["show_new_beneifts"] as? Boolean
+        val showNewBenefits = it.metadata["show_new_benefits"] as? Boolean
         val title = (it.metadata["title_strings"] as? Map<*, *>)?.get("en_US")
         val cta = (it.metadata["cta_strings"] as? Map<*, *>)?.get("en_US")
     }
