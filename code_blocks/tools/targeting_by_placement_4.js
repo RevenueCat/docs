a1379a4f1251253335eeb685af0df1ea48448006
Jeffrey Bunn <jbunn87@gmail.com>
2024-06-07 11:59:43 -0700

    Removed compatibility warning and added code examples

--- code_blocks/tools/targeting_by_placement_4.js
+++ code_blocks/tools/targeting_by_placement_4.js
@@ -1,5 +1,5 @@
-let offering = await Purchases.getCurrentOfferingForPlacement(inputValue);
-if (offerirng) {
+const offering = await Purchases.getCurrentOfferingForPlacement(inputValue);
+if (offering) {
     // TODO: Show paywall
 } else {
     // TODO: Do nothing or continue on to next view
