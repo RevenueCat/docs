c2bdb9a4f49f8d2c5c11ae06374ff5e702620f00
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-06-26 14:55:27 +0200

    Adds KMP SDK installation instructions.

---
+++ code_blocks/getting-started/installation/kmp_3.kts
@@ -0,0 +1,11 @@
+kotlin {
+    // ...
+    sourceSets {
+        // ...
+        named { it.lowercase().startsWith("ios") }.configureEach {
+            languageSettings {
+                optIn("kotlinx.cinterop.ExperimentalForeignApi")
+            }
+        }
+    }
+}
