99f845726c8f6021ca55fa5e7a4acb48bc4e674a
Will Taylor <wtaylor151@gmail.com>
2024-08-02 10:18:14 -0500

    Add React Native PurchasesAreCompletedBy.MY_APP Code Sample

---
+++ code_blocks/migrating-to-revenuecat/observer-mode_7.js
@@ -0,0 +1,7 @@
+Purchases.configure({
+  apiKey: "your_api_key_here",
+  purchasesAreCompletedBy: {
+    type: PURCHASES_ARE_COMPLETED_BY_TYPE.MY_APP,
+    storeKitVersion: STOREKIT_VERSION.STOREKIT_2,
+  },
+});
