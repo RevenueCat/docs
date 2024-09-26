d77c9e755eb6c62bc3f4f090dc99b729be555ff7
Cesar de la Vega <cesarvegaro@gmail.com>
2024-09-26 11:42:22 +0200

    make ids legible

--- code_blocks/tools/customer-center-events-modifier.swift
+++ code_blocks/tools/customer-center-events-modifier.swift
@@ -1,13 +1,16 @@
-.presentCustomerCenter(isPresented: self.$presentingCustomerCenter,
-                       customerCenterActionHandler: { action in
-            switch action {
-            case .restoreStarted:
-            case .restoreFailed(_):
-            case .restoreCompleted(_):
-            case .showingManageSubscriptions:
-            case .refundRequestStarted(_):
-            case .refundRequestCompleted(_):
-            }
-        }) {
-            self.presentingCustomerCenter = false
+.presentCustomerCenter(
+    isPresented: self.$presentingCustomerCenter,
+    customerCenterActionHandler: { action in
+        switch action {
+        case .restoreCompleted(let customerInfo):
+        case .restoreStarted:
+        case .restoreFailed(let error):
+        case .showingManageSubscriptions:
+        case .refundRequestStarted(let productId):
+        case .refundRequestCompleted(let status):
+        case .feedbackSurveyCompleted(let surveyOptionID):
         }
+    }
+) {
+    self.presentingCustomerCenter = false
+}
