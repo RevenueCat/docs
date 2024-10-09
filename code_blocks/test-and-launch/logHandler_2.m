3736eacb07417c2ecd92b17ab056f4c5e365abd6
Michael Fogel <michael@revenuecat.com>
2024-10-09 16:15:58 -0700

    Adding setLogHandler documenation.

---
+++ code_blocks/test-and-launch/logHandler_2.m
@@ -0,0 +1,3 @@
+//After setting logHandler var to whatever you would like, set the logHandler accordingly. 
+RCPurchases.logHandler = RCLogHandler;
+[RCPurchases configureWithAPIKey:@<public_sdk_key> appUserID:@<my_app_user_id>];
---
+++ code_blocks/test-and-launch/logHandler_9.js
@@ -0,0 +1,2 @@
+//After setting logHandler var to whatever you would like, set the logHandler accordingly. 
+Purchases.setLogHandler(logHandler);
