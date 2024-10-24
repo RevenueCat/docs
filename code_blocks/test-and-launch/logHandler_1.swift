c5305d6c1e19d693d653338dd2c4b79fdca99a44
Michael Fogel <michael@revenuecat.com>
2024-10-24 10:44:12 -0700

    Added code block example of custom log handler object

--- code_blocks/test-and-launch/logHandler_1.swift
+++ code_blocks/test-and-launch/logHandler_1.swift
@@ -1,3 +1,22 @@
+/* Custom Log handling object that will handle all of our types: debug, info, warn, and error. 
+
+You can edit the message to display whatever you would like for each case. */
+class CustomLogHandler: LogHandler {
+    func log(level: LogLevel, message: String) {
+        switch level {
+        case .debug:
+            print("[DEBUG] \(message)")
+        case .info:
+            print("[INFO] \(message)")
+        case .warn:
+            print("[WARNING] \(message)")
+        case .error:
+            print("[ERROR] \(message)")
+        }
+    }
+}
+
+
 // After configuring a custom log handler, set the `logHandler` property on Purchases
-Purchases.logHandler = logHandler
+Purchases.logHandler = CustomLogHandler()
 Purchases.configure(withAPIKey: <public_sdk_key>, appUserID: <my_app_user_id>)
