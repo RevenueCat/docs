08b45c249b1e810a55862c8aef4c13f900e2e983
Dan Pannasch <dan@revenuecat.com>
2024-12-04 16:27:01 -0500

    Update Tenjin docs to reference the $tenjinId

--- code_blocks/integrations/attribution/tenjin_2.kt
+++ code_blocks/integrations/attribution/tenjin_2.kt
@@ -1,4 +1,9 @@
 //..
 Purchases.configure(this, "public_sdk_key")
 //..
+
+// Automatically collect the $idfa, $idfv, and $ip values
 Purchases.sharedInstance.collectDeviceIdentifiers()
+
+// Set the Tenjin ID
+Purchases.sharedInstance.setTenjinAnalyticsInstallationID();
