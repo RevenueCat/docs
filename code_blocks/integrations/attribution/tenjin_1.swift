4dcc4a952bf33729f5bb2007efd80af18aed2589
Dan Pannasch <104692013+dpannasch@users.noreply.github.com>
2024-12-06 10:38:31 -0500

    Update Tenjin docs to reference the $tenjinId (#535)
    * Update Tenjin docs to reference the $tenjinId
    
    * SDK availability
    
    * Update code_blocks/integrations/attribution/tenjin_1.swift
    
    Co-authored-by: Toni Rico <antonio.rico.diez@revenuecat.com>
    
    * Update code_blocks/integrations/attribution/tenjin_2.kt
    
    Co-authored-by: Toni Rico <antonio.rico.diez@revenuecat.com>
    
    ---------
    
    Co-authored-by: Toni Rico <antonio.rico.diez@revenuecat.com>

--- code_blocks/integrations/attribution/tenjin_1.swift
+++ code_blocks/integrations/attribution/tenjin_1.swift
@@ -2,4 +2,9 @@ import AdSupport
 // ...
 Purchases.configure(withAPIKey: "public_sdk_key")
 // ...
+
+// Automatically collect the $idfa, $idfv, and $ip values
 Purchases.shared.attribution.collectDeviceIdentifiers()
+
+// Set the Tenjin ID
+Purchases.shared.attribution.setTenjinAnalyticsInstallationID(tenjinAnalyticsId)
