0c210ed5840d81f730c0c014f822bf29c593663e
Wes Clark <145713019+WesleyClarkRC@users.noreply.github.com>
2024-12-10 16:46:07 -0600

    removing the awaits from before the configures (#546)

--- code_blocks/getting-started/configuring-sdk_6.js
+++ code_blocks/getting-started/configuring-sdk_6.js
@@ -8,12 +8,12 @@ export default class App extends React.Component {
     Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG);
 
     if (Platform.OS === 'ios') {
-    	await Purchases.configure({ apiKey: <public_apple_api_key> });
+    	Purchases.configure({ apiKey: <public_apple_api_key> });
     } else if (Platform.OS === 'android') {
-    	await Purchases.configure({ apiKey: <public_google_api_key> });
+    	Purchases.configure({ apiKey: <public_google_api_key> });
 
       // OR: if building for Amazon, be sure to follow the installation instructions then:
-    	await Purchases.configure({ apiKey: <public_amazon_api_key>, useAmazon: true });
+    	Purchases.configure({ apiKey: <public_amazon_api_key>, useAmazon: true });
     }
 
   }
