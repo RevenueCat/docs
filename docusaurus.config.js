f4a32fd3fb681885809a2a11ac82fc10c513e214
Cody Kerns <44073103+codykerns@users.noreply.github.com>
2024-12-23 12:38:58 -0500

    Enable edit link (#573)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,10 +57,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           routeBasePath: "/",
           breadcrumbs: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/RevenueCat/docs/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
