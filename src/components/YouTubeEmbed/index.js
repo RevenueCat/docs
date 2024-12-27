8159db320b7f66a7ae89696e1dd045918aaeef6a
Cody Kerns <cody@revenuecat.com>
2024-12-27 17:12:03 -0500

    styling

--- src/components/YouTubeEmbed/index.js
+++ src/components/YouTubeEmbed/index.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./styles.css";
 
 const YouTubeEmbed = ({ videoId, title }) => {
   return (
     <div className="youtube-embed">
-      <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + videoId}
-        title={title}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      />
+      <div className="video-container">
+        <iframe
+          src={"https://www.youtube.com/embed/" + videoId}
+          title={title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 };
