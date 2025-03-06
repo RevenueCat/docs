7ead1dc721aeb026186c689016783f67a8318887
Andy Boedo <andresboedo@gmail.com>
2025-03-06 17:19:53 -0300

    updated purchases-android version to latest major

--- code_blocks/getting-started/installation/android_3.groovy
+++ code_blocks/getting-started/installation/android_3.groovy
@@ -1,2 +1,2 @@
-implementation 'com.revenuecat.purchases:purchases:7.0.0'
-implementation 'com.revenuecat.purchases:purchases-store-amazon:7.0.0'
+implementation 'com.revenuecat.purchases:purchases:8.14.0'
+implementation 'com.revenuecat.purchases:purchases-store-amazon:8.14.0'
