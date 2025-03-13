0832689717b7df7eb578ee737a1fbc5eb9d36120
Will Taylor <wtaylor151@gmail.com>
2025-03-13 13:09:58 -0500

    remove reference to gold

--- code_blocks/virtual-currency/vc-balance-sdk-ios.swift
+++ code_blocks/virtual-currency/vc-balance-sdk-ios.swift
@@ -1,5 +1,5 @@
 // Get the balance of a specific virtual currency
-let goldBalance = customerInfo.virtualCurrencies[<your_virtual_currency_code>]?.balance
+let balance = customerInfo.virtualCurrencies[<your_virtual_currency_code>]?.balance
 
 // Iterate through all virtual currency balances
 for(virtualCurrencyCode, virtualCurrencyInfo) in customerInfo.virtualCurrencies {
