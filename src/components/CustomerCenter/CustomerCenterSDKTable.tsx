bc32352db54c1bdc955c7ab038a21cdd6d0ba533
Joan Cardona <joancardona.17@gmail.com>
2025-03-18 16:28:06 +0100

    Fix required React Native version for customer center

--- src/components/CustomerCenter/CustomerCenterSDKTable.tsx
+++ src/components/CustomerCenter/CustomerCenterSDKTable.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const sdkVersions = {
   ios: "5.14.0",
   android: "8.12.2",
-  reactNative: "8.6.0",
+  reactNative: "8.7.0",
   flutter: "8.6.0",
   kmp: "1.7.0",
 };
