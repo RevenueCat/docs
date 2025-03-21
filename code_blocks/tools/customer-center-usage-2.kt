95f1b7019452eff8ad2cac41572726c9781f0a9a
Cesar de la Vega <cesarvegaro@gmail.com>
2025-03-21 19:13:34 +0100

    fix showcustomercenter

--- code_blocks/tools/customer-center-usage-2.kt
+++ code_blocks/tools/customer-center-usage-2.kt
@@ -1,13 +1,11 @@
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.ShowCustomerCenter
 
-class YourActivity : AppCompatActivity() {
-    companion object {
-        private const val REQUEST_CODE_CUSTOMER_CENTER = 1234
+class MyActivity : ComponentActivity() {
+    private val customerCenter = registerForActivityResult(ShowCustomerCenter()) {
+        // Handle the dismissal
     }
-
-    private fun showCustomerCenter() {
-        val intent = ShowCustomerCenter()
-            .createIntent(this, Unit)
-        startActivityForResult(intent, REQUEST_CODE_CUSTOMER_CENTER)
+    
+    fun showCustomerCenter() {
+        customerCenter.launch()
     }
-} 
+}
