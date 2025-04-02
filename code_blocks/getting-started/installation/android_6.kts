8e05ccd3743131f122e0b9a9f616d682cb9d8c3b
skydoves <skydoves@naver.com>
2025-04-02 22:24:24 +0900

    Apply comments

--- code_blocks/getting-started/installation/android_6.kts
+++ code_blocks/getting-started/installation/android_6.kts
@@ -1,2 +1,2 @@
-implementation("com.revenuecat.purchases:purchases:8.14.0")
-implementation("com.revenuecat.purchases:purchases-store-amazon:8.14.0")
+implementation("com.revenuecat.purchases:purchases:8.15.1")
+implementation("com.revenuecat.purchases:purchases-store-amazon:8.15.1")
