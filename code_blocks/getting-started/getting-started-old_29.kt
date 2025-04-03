cd144b941721dda000863b4567de5c6e7076947d
skydoves <skydoves@naver.com>
2025-04-03 10:36:35 +0900

    Use accurate getCustomerInfoWith API for Kotlin

--- code_blocks/getting-started/getting-started-old_29.kt
+++ code_blocks/getting-started/getting-started-old_29.kt
@@ -1,4 +1,4 @@
-Purchases.sharedInstance.getCustomerInfo({ error -> /* Optional error handling */ }) { customerInfo ->
+Purchases.sharedInstance.getCustomerInfoWith({ error -> /* Optional error handling */ }) { customerInfo ->
   if (customerInfo.entitlements["my_entitlement_identifier"]?.isActive == true) {
     // Grant user "pro" access
   }
