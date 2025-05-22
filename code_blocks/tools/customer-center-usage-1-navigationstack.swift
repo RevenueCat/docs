2968571fef60705841113d3d4ff6a96454e70232
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-05-22 17:35:54 +0200

    fix: update docs for customer center integration (iOS) #835 (#836)
    * docs updated
    
    * typo
    
    * remove extra indentation
    
    * Update docs/tools/customer-center/customer-center-integration-ios.mdx
    
    Co-authored-by: Nihal Gonsalves <nihal@nihalgonsalves.com>
    
    * missing comment
    
    * typo
    
    ---------
    
    Co-authored-by: Nihal Gonsalves <nihal@nihalgonsalves.com>

---
+++ code_blocks/tools/customer-center-usage-1-navigationstack.swift
@@ -0,0 +1,38 @@
+import RevenueCatUI
+...
+
+var body: some View {
+    NavigationStack {
+        HomeView()
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        self.isCustomerCenterPresented = true
+                    } label: {
+                        Image(systemName: "person.crop.circle")
+                    }
+                }
+            }
+    }
+    .navigationDestination(
+        isPresented: isPresented
+    ) {
+        CustomerCenterView(
+            usesNavigationStack: true, // set this to false when using a NavigationView
+            usesExistingNavigation: true,
+            shouldShowCloseButton: false
+        )
+    }
+    .foregroundColor(.white)
+    .sheet(isPresented: $isCustomerCenterPresented) {
+        CustomerCenterView()
+    }
+}
