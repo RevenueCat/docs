9f968c08c5d955e9d87d7e29482ffab73b25161c
Cody Kerns <44073103+codykerns@users.noreply.github.com>
2025-06-02 08:29:42 -0400

    Update SvgIcon.tsx
    Co-authored-by: Nihal Gonsalves <nihal@nihalgonsalves.com>

--- src/theme/CatIcon/SvgIcon.tsx
+++ src/theme/CatIcon/SvgIcon.tsx
@@ -1,22 +1,15 @@
 import { FC } from "react";
 
-// Define SvgIconProps interface
 export interface SvgIconProps extends React.SVGProps<SVGSVGElement> {
-  sx?: React.CSSProperties | React.CSSProperties[];
-  inheritViewBox?: boolean;
 }
 
 // Define a simple SvgIcon component
 export const SvgIcon: FC<SvgIconProps> = ({
   children,
-  sx = {},
-  inheritViewBox,
   ...props
 }) => {
-  const style = Array.isArray(sx) ? Object.assign({}, ...sx) : sx;
-
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" style={style} {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" {...props}>
       {children}
     </svg>
   );
