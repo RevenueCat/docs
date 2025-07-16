2e0386d6d2d29137872638e2ec079a2e23fc569b
Will Taylor <wtaylor151@gmail.com>
2025-07-16 13:21:14 -0500

    update docs for VC android release

--- code_blocks/virtual-currency/vc-balance-sdk-android.kt
+++ code_blocks/virtual-currency/vc-balance-sdk-android.kt
@@ -1,10 +1,29 @@
-// Don't forget to opt in to experimental RevenueCat APIs with 
-// @OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+// Fetch virtual currencies
+// With coroutines
+try {
+    val getVirtualCurrenciesResult: VirtualCurrencies = purchases.awaitGetVirtualCurrencies()
+} catch(error: PurchasesException) {
+    // TODO: Handle error
+}
 
-// Get the balance of a specific virtual currency
-val balance = customerInfo.virtualCurrencies[<your_virtual_currency_code>]?.balance
+// With callbacks
+purchases.getVirtualCurrenciesWith(
+    onError = { error: PurchasesError -> 
+        // TODO: Handle error
+    },
+    onSuccess = { virtualCurrencies: VirtualCurrencies -> 
+        // TODO: Handle virtual currencies
+    },
+)
+
+// Get the details of a specific virtual currency
+val virtualCurrency = virtualCurrencies.all[<your_virtual_currency_code>]
+val balance = virtualCurrency?.balance
+val name = virtualCurrency?.name
+val code = virtualCurrency?.code
+val description = virtualCurrency?.serverDescription
 
 // Iterate through all virtual currency balances
-for((virtualCurrencyCode, virtualCurrencyInfo) in customerInfo.virtualCurrencies) {
+for ((virtualCurrencyCode, virtualCurrencyInfo) in virtualCurrencies.all) {
     println("$virtualCurrencyCode: ${virtualCurrencyInfo.balance}")
 }
