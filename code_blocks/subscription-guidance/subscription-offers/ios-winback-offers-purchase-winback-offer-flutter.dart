7286bea126f97530921457f4095af06cb3a872ad
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-07-18 17:47:55 +0200

    Change variable name to reflect new type

--- code_blocks/subscription-guidance/subscription-offers/ios-winback-offers-purchase-winback-offer-flutter.dart
+++ code_blocks/subscription-guidance/subscription-offers/ios-winback-offers-purchase-winback-offer-flutter.dart
@@ -1,5 +1,5 @@
 try {
-  final customerInfo = await Purchases.purchasePackageWithWinBackOffer(package, selectedWinBackOffer);
+  final purchaseResult = await Purchases.purchasePackageWithWinBackOffer(package, selectedWinBackOffer);
   // TODO: Handle successful purchase in your UI
 } catch (e) {
   print('Win-Back offer purchase failed: $e');
