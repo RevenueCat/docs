a66af1400acd386a65526359ba14d04bfa72c09c
Will Taylor <wtaylor151@gmail.com>
2025-07-21 07:03:10 -0500

    remove unused android code block

--- code_blocks/virtual-currency/vc-android-experimental-api-annotation-example.kt
+++
@@ -1,8 +0,0 @@
-import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
-
-@OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
-fun printVirtualCurrencyBalances() {
-   for((vcCode, virtualCurrencyInfo) in customerInfo.virtualCurrencies) {
-      println("$virtualCurrencyCode: ${virtualCurrencyInfo.balance}")
-   }
-}
