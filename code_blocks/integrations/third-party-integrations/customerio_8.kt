3a1a6145df887efae5c4600ade6a8fe6647794dc
andsolleiro <andres.solleiro@revenuecat.com>
2025-08-06 16:02:43 +0200

    docs: add full customer.io examples

--- code_blocks/integrations/third-party-integrations/customerio_8.kt
+++ code_blocks/integrations/third-party-integrations/customerio_8.kt
@@ -1,5 +1,20 @@
+import android.content.Context
 import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
+import io.customer.sdk.CustomerIO
 
-val customerIoId = "CUSTOMERIO_ID"
-Purchases.sharedInstance.setAttributes(mapOf("\$customerioId" to customerIoId))
+fun setupRevenueCatWithCustomerIo(context: Context) {
+    // Configure RevenueCat first
+    val config = PurchasesConfiguration.Builder(context, "public_sdk_key")
+        .appUserID("your_app_user_id")
+        .build()
+    Purchases.configure(config)
+
+    // Ensure Customer.io is initialized and identify has been called
+    val customerIoId = CustomerIO.instance().identifier
+
+    customerIoId?.let {
+        Purchases.sharedInstance.setAttributes(mapOf("\$customerioId" to it))
+    }
+}
 
--- code_blocks/integrations/third-party-integrations/customerio_8.swift
+++ code_blocks/integrations/third-party-integrations/customerio_8.swift
@@ -1,5 +1,17 @@
 import RevenueCat
+import CustomerIO
 
-let customerIoId = "CUSTOMERIO_ID"
-Purchases.shared.attribution.setAttributes(["$customerioId": customerIoId])
+func setupRevenueCatWithCustomerIo() {
+    // Configure RevenueCat first
+    Purchases.configure(withAPIKey: "public_sdk_key", appUserID: "your_app_user_id")
+
+    // Ensure Customer.io is initialized and identify has been called
+    let customerIoId = CustomerIO.shared.identifier
+
+    if let customerIoId = customerIoId {
+        Purchases.shared.attribution.setAttributes([
+            "$customerioId": customerIoId
+        ])
+    }
+}
 
