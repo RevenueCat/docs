ef18d285c4575bc80ef2f42f7caebfc006540802
Will Taylor <wtaylor151@gmail.com>
2025-09-16 18:39:14 -0500

    use non-deprecated APIs in CC event sample code

--- code_blocks/tools/customer-center-events.swift
+++ code_blocks/tools/customer-center-events.swift
@@ -1,10 +1,28 @@
-CustomerCenterView { customerCenterAction in
-    switch customerCenterAction {
-    case .restoreStarted:
-    case .restoreFailed(_):
-    case .restoreCompleted(_):
-    case .showingManageSubscriptions:
-    case .refundRequestStarted(_):
-    case .refundRequestCompleted(_):
+CustomerCenterView()
+    .onCustomerCenterRestoreStarted {
+        // TODO: Handle restore started
     }
-}
+    .onCustomerCenterRestoreFailed { error in
+        // TODO: Handle error
+    }
+    .onCustomerCenterRestoreCompleted { customerInfo in
+        // TODO: Handle restore completed
+    }
+    .onCustomerCenterShowingManageSubscriptions {
+        // TODO: Handle showing manage subscriptions
+    }
+    .onCustomerCenterRefundRequestStarted { productId in
+        // TODO: Handle refund request started
+    }
+    .onCustomerCenterRefundRequestCompleted { productId, status in
+        // TODO: Handle refund request completed
+    }
+    .onCustomerCenterFeedbackSurveyCompleted { optionId in
+        // TODO: Handle feedback survey completed
+    }
+    .onCustomerCenterManagementOptionSelected { managementOption in
+        // TODO: Handle Customer Center management option selected
+    }
+    .onCustomerCenterCustomActionSelected { actionIdentifier, purchaseIdentifier in
+        // TODO: Handle custom action selected
+    }
