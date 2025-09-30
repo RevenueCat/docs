f285fc797ff7a8569c0ffa26da7f2ec29b5271c4
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-09-30 10:56:27 +0200

    Removes mentions of purchases-kmp-datetime.

--- code_blocks/getting-started/installation/kmp_2.kts
+++ code_blocks/getting-started/installation/kmp_2.kts
@@ -5,7 +5,6 @@ kotlin {
         commonMain.dependencies {  
             // Add the purchases-kmp dependencies.
             implementation(libs.purchases.core)  
-            implementation(libs.purchases.datetime)   // Optional
             implementation(libs.purchases.either)     // Optional
             implementation(libs.purchases.result)     // Optional
         }
