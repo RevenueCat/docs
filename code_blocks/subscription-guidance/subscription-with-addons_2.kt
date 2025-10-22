a4951e591800ef62021ea000cdbf88154043def0
Tina <tina@revenuecat.com>
2025-10-22 15:20:00 -0700

    initial draft

---
+++ code_blocks/subscription-guidance/subscription-with-addons_2.kt
@@ -0,0 +1,28 @@
+// Fetch available packages with Purchases.sharedInstance.getOfferings()
+
+// You'll need to annotate your function making the purchase with 
+// @OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+// until this feature leaves beta.
+val purchaseParams = PurchaseParams.Builder(
+    activity = currentActivity,
+    packageToPurchase = package1
+)
+    .oldProductId(package1.product.id)
+    .addOnPackages(listOf(package2, package3))
+    .build()
+
+Purchases.sharedInstance.purchase(
+    purchaseParams = purchaseParams,
+    callback = object : PurchaseCallback {
+        override fun onCompleted(
+storeTransaction: StoreTransaction, 
+customerInfo: CustomerInfo
+ ) {
+            // Purchase Completed Successfully
+        }
+
+        override fun onError(error: PurchasesError, userCancelled: Boolean) {
+            // The purchase did not complete successfully
+        }
+    },
+)
