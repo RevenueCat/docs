a941bddd066ab8c6e5f90ebd169efc032769e563
Will Taylor <wtaylor151@gmail.com>
2025-11-05 13:47:24 -0600

    include flutter SDK version + code samples

---
+++ code_blocks/subscription-guidance/subscription-with-addons_flutter_2.dart
@@ -0,0 +1,16 @@
+// Currently only available in Flutter SDK version 9.9.3-add-ons-beta.1
+
+// Fetch available packages to display on your paywall 
+// with Purchases.getOfferings()
+
+// You can also use PurchaseParams.package() or PurchaseParams.subscriptionOption.
+// Additionally, you can provide subscription products as add-ons using the 
+// `addOnSubscriptionOptions()` parameter.
+// Support for add-on packages is coming soon.
+final PurchaseParams params = PurchaseParams.storeProduct(
+  product1,
+  GoogleProductChangeInfo('old_product_id'),
+  addOnStoreProducts: [product2, product3]
+);
+
+final result = await Purchases.purchase(params);
