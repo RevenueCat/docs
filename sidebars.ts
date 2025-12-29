310688d38075659df0d54ae2bd0dcb93eaaf3d20
Daniel Pannasch <danpannasch@gmail.com>
2025-12-29 11:51:44 -0500

    Remove from sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -145,7 +145,6 @@ const paywallsCategory = Category({
           items: [
             Page({ slug: "components" }),
             Page({ slug: "component-properties" }),
-            Page({ slug: "express-checkout" }),
             Page({ slug: "web-purchase-button" }),
             Page({ slug: "variables" }),
             Page({ slug: "localization" }),
