c44b3da75ea0e8ca1caa261e47b29fb23bb571b1
Lim Hoang <limdauto@gmail.com>
2026-01-09 15:06:20 +0000

    Add collectDeviceIdentifiers

--- code_blocks/integrations/attribution/solarengine_1.swift
+++ code_blocks/integrations/attribution/solarengine_1.swift
@@ -4,8 +4,11 @@ import Purchases
 Purchases.configure(withAPIKey: "public_sdk_key")
 // ...
 
+// Collect device identifiers (important for attribution matching)
+Purchases.shared.collectDeviceIdentifiers()
+
 // Retrieve the identifiers from the Solar Engine SDK
-let distinctId = /* Solar Engine SDK Distinct ID */
+let distinctId = /* Solar Engine SDK Distinct ID (optional) */
 let accountId = /* Solar Engine SDK Account ID (optional) */
 let visitorId = /* Solar Engine SDK Visitor ID (optional) */
 
--- code_blocks/integrations/attribution/solarengine_2.kt
+++ code_blocks/integrations/attribution/solarengine_2.kt
@@ -2,8 +2,11 @@
 Purchases.configure(this, "public_sdk_key")
 // ...
 
+// Collect device identifiers (important for attribution matching)
+Purchases.sharedInstance.collectDeviceIdentifiers()
+
 // Retrieve the identifiers from the Solar Engine SDK
-val distinctId = /* Solar Engine SDK Distinct ID */
+val distinctId = /* Solar Engine SDK Distinct ID (optional) */
 val accountId = /* Solar Engine SDK Account ID (optional) */
 val visitorId = /* Solar Engine SDK Visitor ID (optional) */
 
